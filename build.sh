8b1e8e5033b5dd30d10bf9fa7262cd6cebdf82ba
wy <512458266@qq.com>
2023-03-22 12:08:58 +0800

    addd

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # docker system prune
-#tag=20.04-base
-#docker build -t kasmweb/core-ubuntu-focal:develop -f /root/kasm_vnc/workspaces-core-images/dockerfile-kasm-ubuntu   /root/kasm_vnc/workspaces-core-images && docker build -t changhui/ubuntu:$tag -f /root/kasm_vnc/workspaces-images/dockerfile-kasm-desktop /root/kasm_vnc/workspaces-images && docker push changhui/ubuntu:$tag
+tag=20.04-base
+docker build -t kasmweb/core-ubuntu-focal:develop -f ./workspaces-core-images/dockerfile-kasm-ubuntu   /root/kasm_vnc/workspaces-core-images && docker build -t changhui/ubuntu:$tag -f ./workspaces-images/dockerfile-kasm-desktop ./workspaces-images && docker push changhui/ubuntu:$tag
 #exit 0
 tag=20.04-ide
-docker build -t kasmweb/core-ubuntu-focal:develop -f /root/kasm_vnc/workspaces-core-images/dockerfile-kasm-ubuntu   /root/kasm_vnc/workspaces-core-images && docker build -t changhui/ubuntu:$tag -f /root/kasm_vnc/workspaces-images/dockerfile-kasm-desktop-idea /root/kasm_vnc/workspaces-images && docker push changhui/ubuntu:$tag
+docker build -t kasmweb/core-ubuntu-focal:develop -f ./workspaces-core-images/dockerfile-kasm-ubuntu   ./workspaces-core-images && docker build -t changhui/ubuntu:$tag -f ./workspaces-images/dockerfile-kasm-desktop-idea ./workspaces-images && docker push changhui/ubuntu:$tag
 
 
 echo build finish  `date` >>/root/build.txt
