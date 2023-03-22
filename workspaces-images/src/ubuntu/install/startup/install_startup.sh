fafcf3a224ef3d391046af387b65aa8acf0d7a7f
wy <512458266@qq.com>
2023-03-22 22:44:11 +0800

    add

--- workspaces-images/src/ubuntu/install/startup/install_startup.sh
+++ workspaces-images/src/ubuntu/install/startup/install_startup.sh
@@ -10,7 +10,8 @@ mv -f  $HOME/jsmpeg/style.bundle.css /usr/share/kasmvnc/www/dist
 mkdir -p /etc/nginx/conf.d
 mv -f  $HOME/jsmpeg/websocket.conf /etc/nginx/conf.d
 mv -f  $HOME/jsmpeg/nginx.conf /etc/nginx
-
+# 修复新立得安装包以root权限运行
+mv -f $STARTUPDIR/synaptic.desktop /usr/share/applications && chown 1000:0 $STARTUPDIR/synaptic.desktop /usr/share/applications && chmod +x $STARTUPDIR/synaptic.desktop /usr/share/applications/synaptic.desktop
 
 # 安装文件
 apt update &&  apt install -y  fonts-noto-color-emoji
