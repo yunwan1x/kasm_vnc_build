7653bc0bba6ac677fb179443ffe4dfdedaa315d2
changhui <512458266@qq.com>
2023-06-01 05:54:20 +0000

    添加vscode

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -ex
 tag=20.04-base
 docker build -t kasmweb/core-ubuntu-focal:develop -f ./workspaces-core-images/dockerfile-kasm-ubuntu   ./workspaces-core-images \
-&& docker build -t changhui/ubuntu:$tag   -f ./workspaces-images/dockerfile-kasm-desktop ./workspaces-images \
+&& docker build -t changhui/ubuntu:$tag  --progress plain  -f ./workspaces-images/dockerfile-kasm-desktop ./workspaces-images \
 && docker push changhui/ubuntu:$tag
 # exit 0
 tag=20.04-ide
--- workspaces-images/src/ubuntu/install/vs_code/install_vs_code_web.sh
+++ workspaces-images/src/ubuntu/install/vs_code/install_vs_code_web.sh
@@ -5,6 +5,9 @@ wget -O /usr/share/vscode-server-linux-x64-web.tar.gz  https://az764295.vo.msecn
 cd /usr/share
 tar xvzf vscode-server-linux-x64-web.tar.gz
 rm -rf /usr/share/vscode-server-linux-x64-web.tar.gz
+for plugin in ms-python.python ms-toolsai.jupyter MS-CEINTL.vscode-language-pack-zh-hans alefragnani.project-manager PKief.material-icon-theme ;do
+/usr/share/vscode-server-linux-x64-web/bin/code-server --user-data-dir /home/kasm-user  --install-extension $plugin
+done
 # Cleanup
 
 apt-get install -y python3-setuptools \
