a587140c3a72ef4bb5b4dfac46a330fa20887d70
root <512458266@qq.com>
2024-04-17 22:53:03 +0800

    add

--- workspaces-images/src/ubuntu/install/vs_code/install_vs_code_web.sh
+++ workspaces-images/src/ubuntu/install/vs_code/install_vs_code_web.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -ex
-wget -O /usr/share/vscode-server-linux-x64-web.tar.gz  https://az764295.vo.msecnd.net/stable/$(curl https://update.code.visualstudio.com/api/commits/stable/server-linux-x64-web | cut -d '"' -f 2)/vscode-server-linux-x64-web.tar.gz
+wget -O /usr/share/vscode-server-linux-x64-web.tar.gz  https://update.code.visualstudio.com/commit:$(curl https://update.code.visualstudio.com/api/commits/stable/server-linux-x64-web | cut -d '"' -f 2)/server-linux-x64/stable
 cd /usr/share
 tar xvzf vscode-server-linux-x64-web.tar.gz
 rm -rf /usr/share/vscode-server-linux-x64-web.tar.gz
 for plugin in ms-python.python ms-toolsai.jupyter MS-CEINTL.vscode-language-pack-zh-hans alefragnani.project-manager PKief.material-icon-theme alefragnani.bookmarks  eamodio.gitlens cweijan.vscode-office ms-vscode-remote.remote-ssh ms-python.vscode-pylance donjayamanne.python-environment-manager yy0931.vscode-sqlite3-editor;do
-/usr/share/vscode-server-linux-x64-web/bin/code-server   --install-extension $plugin
+/usr/share/vscode-server-linux-x64/bin/code-server   --install-extension $plugin
 done
 # Cleanup
 
+#e170252f762678dec6ca2cc69aba1570769a5d39/
+
+#https://update.code.visualstudio.com/commit:e170252f762678dec6ca2cc69aba1570769a5d39/server-linux-x64/stable
