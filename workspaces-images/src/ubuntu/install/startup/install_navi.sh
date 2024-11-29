f81bfbba7295ab36d2756e77e35aaeed75f6b434
root <512458266@qq.com>
2024-11-29 18:37:51 +0800

    add

--- workspaces-images/src/ubuntu/install/startup/install_navi.sh
+++ workspaces-images/src/ubuntu/install/startup/install_navi.sh
@@ -6,7 +6,16 @@ arch=amd64
 if [[  "$BUILD_ARCH" =~ ^aarch64$ ]] ; then
 arch=arm64
 fi
+
+if [ arm64 = "$arch" ];then
+
+wget  -O /tmp/navi.tgz https://github.com/denisidoro/navi/releases/download/v2.23.0/navi-v2.23.0-aarch64-unknown-linux-gnu.tar.gz
+
+else
 wget  -O /tmp/navi.tgz https://github.com/denisidoro/navi/releases/download/v2.23.0/navi-v2.23.0-$(uname -m)-unknown-linux-musl.tar.gz
+
+fi
+
 tar xvzf /tmp/navi.tgz -C /usr/local/bin && rm -rf /tmp/navi.tgz
 
 wget -O /tmp/fzf.tgz https://github.com/junegunn/fzf/releases/download/v0.55.0/fzf-0.55.0-linux_${arch}.tar.gz
