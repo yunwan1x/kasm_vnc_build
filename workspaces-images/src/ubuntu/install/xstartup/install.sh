0123808048bd40a86f3cda990b787b7a6c85b4e7
root <512458266@qq.com>
2024-12-04 00:31:17 +0800

    add

--- workspaces-images/src/ubuntu/install/xstartup/install.sh
+++ workspaces-images/src/ubuntu/install/xstartup/install.sh
@@ -12,6 +12,7 @@ cp -rf /usr/share/cert $STARTUPDIR/examples
 mv $STARTUPDIR/examples $HOME/Uploads/
 mkdir -p $HOME/.config/xfce4/terminal/
 mv $STARTUPDIR/terminalrc $HOME/.config/xfce4/terminal/
+
 BUILD_ARCH=$(uname -m)
 arch=amd64
 if [[  "$BUILD_ARCH" =~ ^aarch64$ ]] ; then
