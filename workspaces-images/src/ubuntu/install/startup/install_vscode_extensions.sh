f00a13e353be8eb396b5d5e6a7180dda8929b2b9
root <512458266@qq.com>
2025-03-04 14:52:42 +0800

    add

--- workspaces-images/src/ubuntu/install/startup/install_vscode_extensions.sh
+++ workspaces-images/src/ubuntu/install/startup/install_vscode_extensions.sh
@@ -9,13 +9,14 @@ done
 
 for plugin in rangav.vscode-thunder-client johnpapa.vscode-peacock    moshfeu.compare-folders xyz.local-history zjffun.snippetsmanager ;do
 code-server   --install-extension $plugin
+echo install $plugin success
 done
 
-# code-server   --install-extension $STARTUPDIR/vsix/ryu1kn.partial-diff-1.4.3.vsix
-# code-server   --install-extension $STARTUPDIR/vsix/okteto.kubernetes-context-0.1.0.vsix
-# code-server   --install-extension $STARTUPDIR/vsix/touchlab.touchlab-vscode-office-3.1.7.vsix
-# code-server   --install-extension $STARTUPDIR/vsix/rambit.highlight-counter-1.6.0.vsix
-# code-server   --install-extension $STARTUPDIR/vsix/ebicochineal.select-highlight-cochineal-color-0.2.4.vsix
+code-server   --install-extension $STARTUPDIR/vsix/ryu1kn.partial-diff-1.4.3.vsix
+code-server   --install-extension $STARTUPDIR/vsix/okteto.kubernetes-context-0.1.0.vsix
+code-server   --install-extension $STARTUPDIR/vsix/touchlab.touchlab-vscode-office-3.1.7.vsix
+code-server   --install-extension $STARTUPDIR/vsix/rambit.highlight-counter-1.6.0.vsix
+code-server   --install-extension $STARTUPDIR/vsix/ebicochineal.select-highlight-cochineal-color-0.2.4.vsix
 
 
 
