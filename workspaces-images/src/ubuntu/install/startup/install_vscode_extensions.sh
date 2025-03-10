9b77398fe3ac08f2ea0cc7a2daa73b33a8660ed5
root <512458266@qq.com>
2025-03-10 14:26:19 +0800

    add

--- workspaces-images/src/ubuntu/install/startup/install_vscode_extensions.sh
+++ workspaces-images/src/ubuntu/install/startup/install_vscode_extensions.sh
@@ -1,5 +1,5 @@
 
-for plugin in ms-python.python ms-toolsai.jupyter ms-toolsai.jupyter-renderers ms-python.debugpy alefragnani.project-manager PKief.material-icon-theme redhat.vscode-yaml cweijan.vscode-mysql-client2    ms-kubernetes-tools.vscode-kubernetes-tools cweijan.dbclient-jdbc ipedrazas.kubernetes-snippets  hediet.vscode-drawio   ;do
+for plugin in ms-python.python ms-toolsai.jupyter ms-toolsai.jupyter-renderers ms-python.debugpy alefragnani.project-manager PKief.material-icon-theme redhat.vscode-yaml     ms-kubernetes-tools.vscode-kubernetes-tools  ipedrazas.kubernetes-snippets  hediet.vscode-drawio   ;do
 code-server   --install-extension $plugin
 done
 
@@ -17,6 +17,7 @@ code-server   --install-extension $STARTUPDIR/vsix/okteto.kubernetes-context-0.1
 code-server   --install-extension $STARTUPDIR/vsix/touchlab.touchlab-vscode-office-3.1.7.vsix
 code-server   --install-extension $STARTUPDIR/vsix/rambit.highlight-counter-1.6.0.vsix
 code-server   --install-extension $STARTUPDIR/vsix/ebicochineal.select-highlight-cochineal-color-0.2.4.vsix
+code-server   --install-extension $STARTUPDIR/vsix/jry.airdb-1.2.3.vsix
 
 
 
